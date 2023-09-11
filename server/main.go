cf965928f02027e9ac2047e527cb417ba9eaf09b
Enindu Alahapperuma <enindu@gmail.com>
2023-09-12 03:15:38 +0530

    Add improvements

--- server/main.go
+++ server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/blackjack/webcam"
@@ -15,8 +17,9 @@ const (
 )
 
 var (
-	connections = make(map[*websocket.Conn]bool)
-	frame       = make(chan []byte)
+	connections      = make(map[*websocket.Conn]bool)
+	connectionsMutex = sync.Mutex{}
+	frame            = make(chan []byte)
 )
 
 func main() {
@@ -57,6 +60,7 @@ func readFrame(c *webcam.Webcam) {
 		}
 
 		frame <- frameBytes
+
 		time.Sleep(10 * time.Millisecond)
 	}
 }
@@ -67,13 +71,20 @@ func writeMessage() {
 			continue
 		}
 
+		connectionsMutex.Lock()
+
 		for connection := range connections {
 			exception := connection.WriteMessage(websocket.BinaryMessage, <-frame)
 			if exception != nil {
+				log.Printf("%s is disconnected\n", connection.RemoteAddr().String())
+				connection.Close()
 				delete(connections, connection)
+
 				break
 			}
 		}
+
+		connectionsMutex.Unlock()
 	}
 }
 
@@ -87,7 +98,21 @@ func index(w http.ResponseWriter, r *http.Request) {
 		log.Fatalln(exception)
 	}
 
+	connectionsMutex.Lock()
+
+	for existingConnection := range connections {
+		if strings.Split(existingConnection.RemoteAddr().String(), ":")[0] == strings.Split(connection.RemoteAddr().String(), ":")[0] {
+			log.Printf("%s is disconnected (Existing)\n", existingConnection.RemoteAddr().String())
+			existingConnection.Close()
+			delete(connections, existingConnection)
+		}
+	}
+
+	connectionsMutex.Unlock()
+
 	connections[connection] = true
+
+	log.Printf("%s is connected\n", connection.RemoteAddr().String())
 }
 
 func checkOrigin(r *http.Request) bool {
